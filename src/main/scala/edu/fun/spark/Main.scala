d2b9311b65aa1dcc7ad250e826f42f4bed0e9ee4
thibaud <thibaud@BAT-PC.home>
2023-02-16 01:02:34 +0100

    quick setup

---
+++ src/main/scala/edu/fun/spark/Main.scala
@@ -0,0 +1,14 @@
+package edu.fun.spark
+
+import org.apache.spark.sql.SparkSession
+
+object Main {
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val file = spark.read.textFile(args(0)).cache()
+    val numAs = file.filter(line => line.contains("a")).count()
+    val numBs = file.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
+}
